0516ad1cc7a92928d9df4e53ce16a40b8596e45d
amos03 <amossi@gmail.com>
2021-06-29 11:50:33 -0400

    meal time method defined and called for each instance

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,11 +1,12 @@
 class Cat
+#2 initializing with 3 instance variables
 def initialize (cat_name, food, meal_time_pref)
     @name=cat_name
     @preferred_food=food
     @meal_time=meal_time_pref
 end
 
-attr_accessor :name, :preferred_food, :meal_time
+attr_reader :name, :preferred_food, :meal_time
 
 #3 creating two instances of Cat with names, food, meal times
 cat1 = Cat.new("Shunra", "goat cutlets",16)
@@ -14,5 +15,22 @@ cat2 = Cat.new("Kalba", "organic cat treats", 8)
 p cat1
 p cat2
 
+#4 return meal time
+def eats_at
+    puts "=============Meal Time=============" 
+    if @meal_time < 12
+        puts "#{@meal_time} AM"
+    elsif @meal_time > 12
+        puts "#{@meal_time -12} PM"
+    elsif @meal_time==0
+        puts "Midnight"
+    else
+        puts "Current meal time is invalid."
+    end
+    puts "==================================="
+end
+
+cat1.eats_at
+cat2.eats_at
 
 end
