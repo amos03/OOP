8340c79ebc27b74262c078246ff8bc656e76f44e
amos03 <amossi@gmail.com>
2021-06-29 11:06:52 -0400

    creating two instances of Cat

--- cat/cat.rb
+++ cat/cat.rb
@@ -4,3 +4,14 @@ def initialize (cat_name, food, meal_time_pref)
     @preferred_food=food
     @meal_time=meal_time_pref
 end
+
+attr_accessor :name, :preferred_food, @meal_time
+
+cat1 = Cat.new
+cat2 = Cat.new
+
+cat1.name=("Shunra")
+cat2.name=("Kalba")
+
+
+end
