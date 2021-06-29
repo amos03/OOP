ee62d80f6b1bb1cb6ec8d1c91a1b0bfb5d3e51be
amos03 <amossi@gmail.com>
2021-06-29 14:37:50 -0400

    found mistake that led to increase in earnings when quota unmet rather than decreasing by 2, due to redundant negative sign

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -3,30 +3,29 @@ class Paperboy
 def initialize (paperboy_name)
     @name=paperboy_name
     @experience=0
-    @earnings
-    @day_earnings=0
+    @earnings=0
 end
 
-attr_accessor :name, :experience, :day_earnings
+attr_accessor :name, :experience
 attr_reader :earnings
 
 def quota
-    return 50 + 0.5*@experience
+    0.5*@experience + 50
 end
 
 def deliver(start_address,end_address)
-    if end_address - start_address < quota
-        @experience = end_address-start_address
-        return day_earnings= -2
+    if (end_address - start_address) < quota
+        @experience += end_address-start_address
+        @earnings -= 2
     else
-        @day_earnings=(0.25*quota + 0.5*(end_address-start_address-quota))
-        @experience = end_address-start_address
-        return @day_earnings.to_f
+        @earnings+=(0.25*quota + 0.5*(end_address-start_address-quota))
+        @experience += end_address-start_address
+        @earnings.to_f
     end
 end
 
 def report
-    puts "I'm #{@name}, I've delivered #{@experience} papers and earned $#{day_earnings} so far."
+    puts "I'm #{@name}, I've delivered #{@experience} papers and earned $#{@earnings} so far."
 end
 
 p paperboy1 = Paperboy.new("Shalom")
@@ -35,4 +34,13 @@ paperboy1.deliver(200,2000)
 
 paperboy1.report
 
+p paperboy1
+
+p paperboy1.quota
+
+paperboy1.deliver(100,200)
+
+paperboy1.report
+
+p paperboy1
 end
