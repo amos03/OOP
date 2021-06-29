2e1afc2b4501b4dfa0fedd2b15b0ee930c67d3b0
amos03 <amossi@gmail.com>
2021-06-29 14:16:11 -0400

    working but misunderstanding earnings read only

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -4,9 +4,10 @@ def initialize (paperboy_name)
     @name=paperboy_name
     @experience=0
     @earnings
+    @day_earnings=0
 end
 
-attr_accessor :name, :experience
+attr_accessor :name, :experience, :day_earnings
 attr_reader :earnings
 
 def quota
@@ -15,10 +16,12 @@ end
 
 def deliver(start_address,end_address)
     if end_address - start_address < quota
-        day_earnings = -2
+        @experience = end_address-start_address
+        return day_earnings= -2
     else
-        day_earnings=0.25*quota + 0.5*(end_address-start_address-quota)
-        return day_earnings.to_f
+        @day_earnings=(0.25*quota + 0.5*(end_address-start_address-quota))
+        @experience = end_address-start_address
+        return @day_earnings.to_f
     end
 end
 
@@ -28,6 +31,8 @@ end
 
 p paperboy1 = Paperboy.new("Shalom")
 
-report.paperboy1
+paperboy1.deliver(200,2000)
+
+paperboy1.report
 
 end
